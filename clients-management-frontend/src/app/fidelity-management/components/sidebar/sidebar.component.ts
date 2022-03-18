ea4d4337e46677791f97d2e5c2dc6ed5c0fdbaf5
vscalcione <vincenzo99.scalcione@gmail.com>
2022-03-18 21:06:16 +0100

    Update sidebar component

--- clients-management-frontend/src/app/fidelity-management/components/sidebar/sidebar.component.ts
+++ clients-management-frontend/src/app/fidelity-management/components/sidebar/sidebar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  items = [
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' },
+    { name: 'Item 4' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

6cb892da46e7efb86a8fda6a3241d7148aff40a6
vscalcione <vincenzo99.scalcione@gmail.com>
2022-03-21 13:05:30 +0100

    Update dotnet project

--- clients-management-webapi/Program.cs
+++ clients-management-webapi/Program.cs
@@ -1,13 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+IConfiguration configuration = app.Configuration;
+var connection = configuration["ConnectionStrings:clientsManagementDbConnectionString"];
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,10 +19,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options => options
+    .WithOrigins("https://localhost:4200")
+    .WithMethods("POST", "PUT", "DELETE", "GET")
+    .AllowAnyHeader()
+);
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
